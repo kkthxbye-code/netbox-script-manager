34c6df44fef3e9d908ce18fab5fa544b1808a49c
kkthxbye-code <festll234@gmail.com>
2023-10-16 13:39:34 +0200

    Bump version: 0.3.5 → 0.3.6

--- netbox_script_manager/__init__.py
+++ netbox_script_manager/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Simon Toft"""
 __email__ = "festll234@gmail.com"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 
 from extras.plugins import PluginConfig
@@ -12,7 +12,7 @@ class NetboxScriptManagerConfig(PluginConfig):
     name = "netbox_script_manager"
     verbose_name = "Script Manager"
     description = "Improved custom script support for netbox"
-    version = "0.3.5"
+    version = "0.3.6"
     base_url = "script-manager"
     default_settings = {
         "DEFAULT_QUEUE": "default",
